781842c3b8ac0820ed64a1a6f1ec7a22e68107d2
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-04-05 23:51:54 +0800

    extra render helper

--- src/main/java/dev/xkmc/golemmagicka/events/ClientGolemEventHandlers.java
+++ src/main/java/dev/xkmc/golemmagicka/events/ClientGolemEventHandlers.java
@@ -4,14 +4,18 @@ import dev.xkmc.golemmagicka.content.entity.GolemSpellManager;
 import dev.xkmc.golemmagicka.init.GolemMagicka;
 import dev.xkmc.golemmagicka.init.data.GMLang;
 import dev.xkmc.golemmagicka.util.SpellCategoryUtil;
+import dev.xkmc.modulargolems.content.entity.common.AbstractGolemEntity;
 import dev.xkmc.modulargolems.events.event.GolemInfoEvent;
 import io.redspace.ironsspellbooks.api.entity.IMagicEntity;
 import io.redspace.ironsspellbooks.api.registry.AttributeRegistry;
 import io.redspace.ironsspellbooks.api.registry.SpellRegistry;
+import io.redspace.ironsspellbooks.player.ClientMagicData;
+import io.redspace.ironsspellbooks.render.SpellRenderingHelper;
 import net.minecraft.network.chat.Component;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.RenderLivingEvent;
 
 @EventBusSubscriber(value = Dist.CLIENT, modid = GolemMagicka.MODID)
 public class ClientGolemEventHandlers {
@@ -39,6 +43,13 @@ public class ClientGolemEventHandlers {
 			for (var spell : invalid) {
 				event.addLine(GMLang.INVALID_SPELL.get(Component.translatable(spell.getComponentId())));
 			}
+		}
+	}
+
+	@SubscribeEvent
+	public static void renderGolem(RenderLivingEvent.Post<?, ?> event) {
+		if (event.getEntity() instanceof AbstractGolemEntity<?, ?> golem) {
+			SpellRenderingHelper.renderSpellHelper(ClientMagicData.getSyncedSpellData(golem), golem, event.getPoseStack(), event.getMultiBufferSource(), event.getPartialTick());
 		}
 	}
 
